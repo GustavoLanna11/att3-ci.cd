44bc2da6a5999ffba4362ec751cc35af1812b94d
Gustavo Lanna <gustavomlanna@gmail.com>
2025-11-21 16:16:44 -0300

    wrench: ajustes no github workflow

--- src/test/java/com/biblioteca/controller/LivroControllerTest.java
+++ src/test/java/com/biblioteca/controller/LivroControllerTest.java
@@ -1,5 +1,6 @@
 package com.biblioteca.controller;
 
+import com.biblioteca.exception.GlobalExceptionHandler;
 import com.biblioteca.model.Livro;
 import com.biblioteca.service.LivroService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,13 +18,13 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LivroController.class)
+@Import(GlobalExceptionHandler.class)
 class LivroControllerTest {
 
     @Autowired
